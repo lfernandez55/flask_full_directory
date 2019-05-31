577290759dc57d4f37ea83bae825254bf4979c7b
lfernandez55 <lfernandez@weber.edu>
2019-05-31 15:37:01 -0600

    Commented out main_views code

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,9 +1,9 @@
 # __init__.py is a special Python file that allows a directory to become
 # a Python package so it can be accessed using the 'import' statement.
 
-from .main_views import main_blueprint
+# from .main_views import main_blueprint #(no longer used)
 from .book_views import book_blueprint
 
 def register_blueprints(app):
-    app.register_blueprint(main_blueprint)
+    # app.register_blueprint(main_blueprint) #(no longer used)
     app.register_blueprint(book_blueprint)
